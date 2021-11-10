09be97f65d7517555aea1a97d7b7d3256caa594b
WilberEnriqueSeguraRamirez <71159717+WilberEnriqueSeguraRamirez@users.noreply.github.com>
2021-11-10 07:16:34 -0600

    Proyecto

---
+++ ProyectoHSC22021/Modulos/Contabilidad/Requerimiento Cierre Contable/ControladorCierreContable/Controlador.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Odbc;
+
+namespace ControladorCierreContable
+{
+    class Controlador//Hecho por Wilber Enrique Segura Ramirez 0901-18-13952
+    {
+        public OdbcConnection conexion()
+        {
+            //creacion de la conexion via ODBC
+            OdbcConnection conn = new OdbcConnection("Dsn=conexionHSC");
+            try
+            {
+                conn.Open();
+            }
+            catch (OdbcException)
+            {
+                Console.WriteLine("No se establecio Conexion!");
+            }
+            return conn;
+        }
+
+        //metodo para cerrar la conexion
+        public void desconexion(OdbcConnection conn)
+        {
+            try
+            {
+                conn.Close();
+            }
+            catch (OdbcException)
+            {
+                Console.WriteLine("No se establecio Conexion!");
+            }
+        }
+    }
+}
---
+++ ProyectoHSC22021/Modulos/Contabilidad/Requerimiento Cierre Contable/VistaCierreContable/frmIngreso.Designer.cs
@@ -0,0 +1,83 @@
+﻿
+namespace VistaCierreContable
+{
+    partial class frmIngreso
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(221, 44);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(163, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ingrese Fecha del Cierre";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(362, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(143, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Formato año-mes-día";
+            // 
+            // frmIngreso
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "frmIngreso";
+            this.Text = "7602 Ingreso Cierre";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
